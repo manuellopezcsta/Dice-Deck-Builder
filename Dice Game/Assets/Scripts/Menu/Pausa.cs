c1b158d4f552e813f1ca9ec254509e9efb0da383
Gaston Trombotti <gastontrombotti@gmail.com>
2023-10-05 17:20:35 -0300

    Menu de pausa durante el juego (presionar 'escape') falta la funcionalidad de los botones de opciones, volver al menu principal y salir

---
+++ Dice Game/Assets/Scripts/Menu/Pausa.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Pausa : MonoBehaviour
+{
+    public static bool juegoPausado = false;
+
+    public GameObject menuPausa;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(juegoPausado){
+                Resumir();
+            }else{
+                Pausar();
+            }
+        }
+    }
+    public void Resumir(){
+        menuPausa.SetActive(false);
+        Time.timeScale=1f;
+        juegoPausado=false;
+    }
+
+    void Pausar(){
+        menuPausa.SetActive(true);
+        Time.timeScale=0f;
+        juegoPausado=true;
+    }
+
+    public void AbrirOpciones(){
+
+    }
+
+    public void IrAMenuPrincipal () {
+        
+    }
+}
