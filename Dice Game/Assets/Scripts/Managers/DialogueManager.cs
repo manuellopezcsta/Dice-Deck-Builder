fa1e4dede2c377eb7f4cf6758433d6f678cb0b64
Gaston Trombotti <gastontrombotti@gmail.com>
2023-10-17 23:31:02 -0300

    Sistema de textbox completado, se podria agregar el control de velocidad a opciones por el momento esta ligado a un boton de prueba para testeo pero cualquier objeto puede agregarse con el script DialogueTrigger

--- Dice Game/Assets/Scripts/Managers/DialogueManager.cs
+++ Dice Game/Assets/Scripts/Managers/DialogueManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows;
+using UnityEngine.UI;
+using TMPro;
 
 //Manager para dialogo 
 public class DialogueManager : MonoBehaviour
@@ -10,6 +12,12 @@ public class DialogueManager : MonoBehaviour
     private Queue<String> sentences; //guarda el dialogo en una fila para acceder luego
     // Start is called before the first frame update
     private bool inDialogue = false;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public Animator animator;
+
+    public int dialogueSpeed;
     void Start()
     {
         sentences = new Queue<string>();
@@ -24,25 +32,43 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue (Dialogue dialogue) {
         inDialogue=true;
-        Debug.Log("Starting conversation with "+dialogue.name);
+        animator.SetBool("IsOpen", true);
+        //Debug.Log("Starting conversation with "+dialogue.name);
+        nameText.text=dialogue.name;
         sentences.Clear();
         foreach(string sentence in dialogue.sentences){
             sentences.Enqueue(sentence);
         }
+        
         DisplayNextSentence();
     }
 
-    public void DisplayNextSentence () {
+    public void DisplayNextSentence () { //Texto instantaneo
         if(sentences.Count == 0){
             EndDialogue();
             return;
         }
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        //Debug.Log(sentence);
+        //dialogueText.text = sentence; //Texto instantaneo
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence){ //Texto letra por letra
+        dialogueText.text="";
+        foreach(char letter in sentence.ToCharArray()){
+            dialogueText.text+=letter;
+
+            for(int i = 0; i < dialogueSpeed; i++) {
+                yield return null;    //espera dialogueSpeed frames entre cada letra
+            }
+        }
     }
 
     void EndDialogue(){
         inDialogue=false;
+        animator.SetBool("IsOpen", false);
         Debug.Log("End conversation");
     }
     
