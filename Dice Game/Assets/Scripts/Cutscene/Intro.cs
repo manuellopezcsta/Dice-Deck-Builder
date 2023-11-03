152dd6a88743f005ea2e9328c986e5c065f466a3
Gaston Trombotti <gastontrombotti@gmail.com>
2023-11-03 17:03:27 -0300

    Beta version of intro cinematic

---
+++ Dice Game/Assets/Scripts/Cutscene/Intro.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    public TextMeshProUGUI date;
+    //private string strBase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AnimateText("3294 D.C."));
+    }
+
+    IEnumerator AnimateText(string text){
+        for(int i = 0; i < text.Length; i++) {
+            date.text+=text[i];
+            yield return new WaitForSeconds(0.5f);
+            Debug.Log(date.text);
+        }
+    }
+}
